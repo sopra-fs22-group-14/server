0bed260365ac14b29609e6cf264089791fb1e0b7
Diego Bugmann <digi.bugi@hotmail.com>
2022-04-07 19:15:02 +0200

    authorization change (not fully working yet)

--- src/main/java/ch/uzh/ifi/hase/soprafs22/controller/UserController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/controller/UserController.java
@@ -74,10 +74,11 @@ public class UserController {
     @PostMapping("users/logout")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @ResponseBody
-    public UserLoginDTO logout(@RequestHeader(name="token") String token) {
+    public UserLoginDTO logout(@RequestHeader("Authorization") String token) {
         // return same things as in registration
         // ! logout token sent via header
         // ! set token to empty string on logout
+        System.out.println("TOKEN: "+token);
         User userByToken=userService.logout(token);
         return DTOMapper.INSTANCE.convertEntityToUserLoginDTO(userByToken);
     }
