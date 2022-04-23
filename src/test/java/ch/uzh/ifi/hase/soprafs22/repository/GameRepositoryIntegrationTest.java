cd43fbf44f207a843826a58b121d84a0a32752e7
egeonurgulec <onur.guleec@boun.edu.tr>
2022-04-23 17:13:28 +0200

    fixed gamerepo test

--- src/test/java/ch/uzh/ifi/hase/soprafs22/repository/GameRepositoryIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs22/repository/GameRepositoryIntegrationTest.java
@@ -3,6 +3,7 @@ package ch.uzh.ifi.hase.soprafs22.repository;
 import ch.uzh.ifi.hase.soprafs22.entity.Game;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
@@ -15,6 +16,8 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+
 public class GameRepositoryIntegrationTest {
 
     @Autowired
