c6a80be7b844e51ded34c72f09d041956e6f5297
Diego Bugmann <digi.bugi@hotmail.com>
2022-05-28 15:51:47 +0200

    implemented service integration tests

---
+++ src/test/java/ch/uzh/ifi/hase/soprafs22/service/GameServiceIntegrationTest.java
@@ -0,0 +1,124 @@
+package ch.uzh.ifi.hase.soprafs22.service;
+
+import ch.uzh.ifi.hase.soprafs22.entity.Game;
+import ch.uzh.ifi.hase.soprafs22.entity.Player;
+import ch.uzh.ifi.hase.soprafs22.entity.User;
+import ch.uzh.ifi.hase.soprafs22.repository.GameRepository;
+import ch.uzh.ifi.hase.soprafs22.repository.PlayerRepository;
+import ch.uzh.ifi.hase.soprafs22.repository.UserRepository;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebAppConfiguration
+@SpringBootTest
+public class GameServiceIntegrationTest {
+
+  // Using the REAL repositories
+  @Qualifier("gameRepository")
+  @Autowired
+  private GameRepository gameRepository;
+
+  @Autowired
+  private GameService gameService;
+
+  @Qualifier("userRepository")
+  @Autowired
+  private UserRepository userRepository;
+  @Autowired
+  private UserService userService;
+
+  @Qualifier("playerRepository")
+  @Autowired
+  private PlayerRepository playerRepository;
+
+  private User testUser;
+  private User testUser2;
+  private Game testGame;
+
+  @BeforeEach
+  public void setup() {
+    // add a user to the db (needed to create a new game)
+    testUser = new User();
+    testUser.setUsername("test1234user9876name0011");
+    testUser.setPassword("testPassword");
+    testUser.setToken("testToken");
+    testUser = userService.createUser(testUser);
+
+    Game testGame = new Game();
+    testGame.setNumOfRounds(8);
+    testGame.setGameEdition("regular");
+    testGame.setGameName("testGame1234abcd5678efgh");
+    testGame.setCardCzarMode(true);
+    Game createdGame = gameService.createNewGame(testGame, testUser.getToken());
+    Game foundGame = gameRepository.findByGameId(createdGame.getGameId());
+    this.testGame = foundGame;
+  }
+
+
+  @Test
+  public void test_createGame_integration() {
+
+    assertNotNull(gameRepository.findByGameName("testGame1234abcd5678efgh"));
+
+    assertEquals(8, testGame.getNumOfRounds());
+    assertEquals("regular", testGame.getGameEdition());
+    assertEquals("testGame1234abcd5678efgh", testGame.getGameName());
+    assertEquals(true, testGame.isCardCzarMode());
+
+  }
+
+  @Test
+  public void test_joinGame_integration() {
+
+    testUser2 = new User();
+    testUser2.setUsername("test1234user9876name0012");
+    testUser2.setPassword("testPassword");
+    testUser2.setToken("testTokenn");
+    testUser2 = userService.createUser(testUser2);
+
+    assertNotNull(gameRepository.findByGameName("testGame1234abcd5678efgh"));
+
+    Game joinedGame = gameService.joinGame(testGame.getGameId(), testUser2.getToken());
+    Player joinedPlayer = playerRepository.findByPlayerId(testUser2.getUserId());
+    assertEquals(joinedGame.getGameId(), joinedPlayer.getCurrentGameId());
+    assertEquals(2, joinedGame.getNumOfPlayersJoined());
+
+  }
+
+  @Test
+  public void test_leaveGameAndDelete_integration() {
+
+    assertNotNull(gameRepository.findByGameName("testGame1234abcd5678efgh"));
+
+    gameService.leaveGame(testGame.getGameId(), testUser.getToken());
+
+    // check if player and game were deleted
+    assertNull(gameRepository.findByGameId(testGame.getGameId()));
+    assertNull(gameRepository.findByGameName("testGame1234abcd5678efgh"));
+    assertNull(playerRepository.findByPlayerId(testUser.getUserId()));
+    assertNotNull(userRepository.findByUserId(testUser.getUserId()));
+
+    userRepository.delete(testUser);
+
+    testUser = null;
+
+  }
+
+  @AfterEach
+  public void cleanUp() {
+    if (testGame != null) gameRepository.delete(testGame);
+    if (testUser != null) {
+      playerRepository.deleteById(testUser.getUserId());
+      userRepository.delete(testUser);
+    }
+    if (testUser2 != null) {
+      playerRepository.deleteById(testUser2.getUserId());
+      userRepository.delete(testUser2);
+    }
+  }
+}
--- src/test/java/ch/uzh/ifi/hase/soprafs22/service/UserServiceIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs22/service/UserServiceIntegrationTest.java
@@ -1,17 +1,32 @@
 package ch.uzh.ifi.hase.soprafs22.service;
 
 import ch.uzh.ifi.hase.soprafs22.constant.UserStatus;
+import ch.uzh.ifi.hase.soprafs22.controller.UserController;
+import ch.uzh.ifi.hase.soprafs22.entity.Player;
 import ch.uzh.ifi.hase.soprafs22.entity.User;
+import ch.uzh.ifi.hase.soprafs22.entity.UserTimestamp;
+import ch.uzh.ifi.hase.soprafs22.repository.PlayerRepository;
 import ch.uzh.ifi.hase.soprafs22.repository.UserRepository;
+import ch.uzh.ifi.hase.soprafs22.repository.UserTimestampRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
 
 /**
  * Test class for the UserResource REST resource.
@@ -29,50 +44,116 @@ public class UserServiceIntegrationTest {
   @Autowired
   private UserService userService;
 
+  private User testUser;
+
+  /**
+  In our case, deleting the entire database does not make sense, because we do not use H2, but we use a
+   persistent database. What we do here is create a dummy user with a highly unlikely username, do some
+   tests and delete this user again in the end.
+   */
+
   @BeforeEach
   public void setup() {
-    userRepository.deleteAll();
+    testUser = new User();
+    testUser.setUsername("hjsffgydvsdcfsd2234453sdf");
+    testUser.setPassword("testPassword");
+    testUser.setTimesPicked(0);
+    testUser = userService.createUser(testUser);
   }
 
   @Test
   public void createUser_validInputs_success() {
-    // given
-    assertNull(userRepository.findByUsername("testUsername"));
 
-    User testUser = new User();
-    testUser.setUsername("testUsername");
-    testUser.setPassword("testPassword");
-    testUser.setTimesPicked(0);
-    // when
-    User createdUser = userService.createUser(testUser);
+    // given
+    assertNotNull(userRepository.findByUsername("hjsffgydvsdcfsd2234453sdf"));
 
     // then
-    assertEquals(testUser.getUserId(), createdUser.getUserId());
-    assertEquals(testUser.getUsername(), createdUser.getUsername());
-    assertEquals(testUser.getPassword(), createdUser.getPassword());
-    assertNotNull(createdUser.getToken());
-    assertEquals(UserStatus.ONLINE, createdUser.getStatus());
+    assertEquals(testUser.getUserId(), testUser.getUserId());
+    assertEquals(testUser.getUsername(), testUser.getUsername());
+    assertEquals(testUser.getPassword(), testUser.getPassword());
+    assertEquals(testUser.getTimesPicked(), testUser.getTimesPicked());
+    assertNotNull(testUser.getToken());
+    assertEquals(UserStatus.ONLINE, testUser.getStatus());
   }
 
   @Test
   public void createUser_duplicateUsername_throwsException() {
-    assertNull(userRepository.findByUsername("testUsername"));
-
-    User testUser = new User();
-    testUser.setUsername("testUsername");
-    testUser.setPassword("testPassword");
-    testUser.setTimesPicked(0);
-
-    User createdUser = userService.createUser(testUser);
+    assertNotNull(userRepository.findByUsername("hjsffgydvsdcfsd2234453sdf"));
 
     // attempt to create second user with same username
     User testUser2 = new User();
 
     // change the name but forget about the username
-    testUser2.setUsername("testUsername");
+    testUser2.setUsername("hjsffgydvsdcfsd2234453sdf");
     testUser2.setPassword("testPassword");
 
     // check that an error is thrown
     assertThrows(ResponseStatusException.class, () -> userService.createUser(testUser2));
   }
+
+  @Test
+  public void login_success() {
+    assertNotNull(userRepository.findByUsername("hjsffgydvsdcfsd2234453sdf"));
+
+    userService.login(testUser);
+    assertEquals(UserStatus.ONLINE, userRepository.findByUserId(testUser.getUserId()).getStatus());
+  }
+
+  @Test
+  public void logout_success() {
+    assertNotNull(userRepository.findByUsername("hjsffgydvsdcfsd2234453sdf"));
+
+    userService.logout(testUser.getToken());
+    assertEquals(UserStatus.OFFLINE, userRepository.findByUserId(testUser.getUserId()).getStatus());
+  }
+
+  @Test
+  public void checkIfAuthorized_success() {
+    assertNotNull(userRepository.findByUsername("hjsffgydvsdcfsd2234453sdf"));
+    assertDoesNotThrow(() -> userService.checkIfAuthorized(testUser.getToken()));
+  }
+
+  @Test
+  public void checkIfAuthorized_failure() {
+    assertNotNull(userRepository.findByUsername("hjsffgydvsdcfsd2234453sdf"));
+    assertThrows(ResponseStatusException.class, () -> userService.checkIfAuthorized("wrongToken"));
+  }
+
+  @Test
+  public void getUser() {
+    assertNotNull(userRepository.findByUsername("hjsffgydvsdcfsd2234453sdf"));
+    User foundUser = userService.getUser(testUser.getToken());
+    assertEquals(foundUser.getUserId(), testUser.getUserId());
+    assertEquals(foundUser.getToken(), testUser.getToken());
+  }
+
+  @Test
+  public void changeUserProfile_success() {
+    assertNotNull(userRepository.findByUsername("hjsffgydvsdcfsd2234453sdf"));
+
+    Date newBirthday = new Date();
+    String newUsername = "aaaqqqgggbbbtttwwwkkklll";
+
+    userService.changeUserProfile(testUser.getToken(), newUsername, newBirthday, testUser.getPassword());
+
+    User db_user = userRepository.findByUserId(testUser.getUserId());
+    assertEquals(newUsername, db_user.getUsername());
+  }
+
+  @Test
+  public void changeUserProfile_failure() {
+    assertNotNull(userRepository.findByUsername("hjsffgydvsdcfsd2234453sdf"));
+
+    Date newBirthday = new Date();
+    String newUsername = "aaaqqqgggbbbtttwwwkkklll";
+
+    assertThrows(ResponseStatusException.class,
+            () -> userService.changeUserProfile(testUser.getToken(), newUsername, newBirthday, "wrongPassword"));
+  }
+
+  @AfterEach
+  public void cleanUp() {
+    if (testUser != null) userRepository.deleteById(testUser.getUserId());
+  }
+
 }
